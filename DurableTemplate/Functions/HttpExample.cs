985af648b14064061fac1ad9e0be2aa279478211
Simon Crouch <simon.crouch@felinesoft.com>
2020-08-11 21:40:18 +0100

    upgrade packages

--- DurableTemplate/Functions/HttpExample.cs
+++ DurableTemplate/Functions/HttpExample.cs
@@ -27,7 +27,7 @@ namespace DurableTemplate.Functions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            string? name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
