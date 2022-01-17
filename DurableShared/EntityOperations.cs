aad9468fc3f563d047b5eff30d9e285d620c59a2
Simon Crouch <iamontheinternet@yahoo.com>
2022-01-17 21:27:43 +0000

    add entity operations

--- DurableShared/EntityOperations.cs
+++ DurableShared/EntityOperations.cs
@@ -1,7 +1,17 @@
-﻿namespace DurableShared
-{
-    public class Class1
-    {
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 
+namespace DurableShared;
+
+public class EntityOperations
+{
+    public async Task<EntityStateResponse<T>> GetEntityAsync<T>(IDurableEntityClient client, string uniqueKey) where T : class
+    {
+        var entityId = GetEntityId<T>(uniqueKey);
+        return await client.ReadEntityStateAsync<T>(entityId);
+    }
+
+    public static EntityId GetEntityId<T>(string uniqueKey) where T : class
+    {
+        return new EntityId(nameof(T), uniqueKey);
     }
 }
